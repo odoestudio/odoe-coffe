20f7aa7801ceda2fa5e8744e63d9b77648f8b6cb
odoestudio <odoestudiodev@gmail.com>
2025-07-30 22:17:22 -0700

    Add data.js via Odoe

---
+++ data.js
@@ -0,0 +1,61 @@
+const users = [
+    {
+        id: 1,
+        name: 'Alex', 
+        age: 28,
+        image: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80',
+        bio: 'Just a dude who likes coffee more than people. Probably.',
+        backgroundCheck: ['Says they’re 6ft but nah.', 'Always late.'],
+        isCrush: true,
+        rating: 4,
+        notes: 'Great chat, but he brought his own weird tea bags to the coffee shop. Red flag? Or just quirky?'
+    },
+    {
+        id: 2,
+        name: 'Jenna', 
+        age: 25,
+        image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80',
+        bio: 'Looking for someone to steal hoodies from. Must be taller than my plant.',
+        backgroundCheck: ['Ghosted me after one date.', 'Only talks about crypto.'],
+        isCrush: false,
+        rating: 2,
+        notes: 'Conversation was like pulling teeth. Asked me for my star sign then judged me for it.'
+    },
+    {
+        id: 3,
+        name: 'Mikey', 
+        age: 31,
+        image: 'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80',
+        bio: 'My dog thinks I\'m cool. That\'s all that matters.',
+        backgroundCheck: ['Still uses an AOL email address.'],
+        isCrush: false,
+        rating: 5,
+        notes: 'Actually hilarious. We talked for 3 hours. Would date again.'
+    },
+    {
+        id: 4,
+        name: 'Chloe', 
+        age: 27,
+        image: 'https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80',
+        bio: 'Sarcasm is my love language. And pizza.',
+        backgroundCheck: ['Has a sticker on her laptop that says “✨red flag✨”... ironically?'],
+        isCrush: true,
+        rating: 3,
+        notes: 'Funny, but checked her phone a lot. Maybe a busy queen, maybe just rude.'
+    }
+];
+
+const teaFeed = [
+    {
+        user: 'doodle_dork', 
+        comment: 'Just got unmatched bc I said pineapple on pizza is a masterpiece. His loss. 🍍'
+    },
+    {
+        user: 'sad_boi_hours',
+        comment: 'Breakup quote of the day: “Our love was like a broken pencil... pointless.”'
+    },
+    {
+        user: 'spill_the_beans',
+        comment: 'ANON SECRET: Saw my match on another date... while on our date. The audacity.'
+    }
+];
